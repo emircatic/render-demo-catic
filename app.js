b37a482e0a20cb1ef056c4020af63614bd8f716e
Emir Catic <catic.e19@htlwienwest.at>
2023-01-18 22:02:50 +0100

    Helmet Removed

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
-app.use(helmet());
 app.use(morgan('tiny'));
 app.use(express.json());
 app.use('/', immoRouter);
@@ -19,10 +18,9 @@ app.use(express.static('./public'));
 
 const dirname = path.resolve();
 
-app.use(helmet({contentSecurityPolicy: false,}),);
-app.get('*', (req, res) =>
-	res.status(404).sendFile(path.join(dirname, 'error/404-error.webp'))
-);
+app.use(helmet({ contentSecurityPolicy: false }));
+
+app.get('*', (req, res) => res.status(404).sendFile(path.join(dirname, 'error/404-error.webp')));
 
 const PORT = process.env.PORT ?? 5555;
 
